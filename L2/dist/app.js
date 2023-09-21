755e517e1219e88597ef62151616752d3b5e8ee8
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-09-21 19:57:53 +0400

    add jsdoc

---
+++ L2/dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const ali = {
+    name: 'ali',
+    age: 12
+};
+console.log(ali.name);
+function cem() {
+}
+cem();
---
+++ L2/src/app.ts
@@ -0,0 +1,18 @@
+const ali = {
+    name: 'ali',
+    age: 12
+}
+/** xfjcvbn dkflvlncl */
+console.log(ali.name)
+
+// JsDoc 
+/**
+ * @param n1: sadece eded
+ * @param n2: sadece eded
+ */
+function cem() {
+
+}
+
+
+cem()
