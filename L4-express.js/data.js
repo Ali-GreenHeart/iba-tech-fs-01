e7696ee02f385824ac37fe50dfd99ccfa876e9ac
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-09-26 21:14:14 +0400

    initial rest api codes

---
+++ L4-express.js/data.js
@@ -0,0 +1,30 @@
+const users = [
+    {
+        "id": "0",
+        "name": "Miri",
+        "job": "express dev"
+    },
+    {
+        "id": "1",
+        "name": "alion",
+        "job": "node.js"
+    },
+    {
+        "id": "2",
+        "name": "Tunzale",
+        "job": "react dev"
+    },
+    {
+        "id": "3",
+        "name": "sara",
+        "job": "fullstack"
+    },
+    {
+        "id": "4",
+        "name": "tupack",
+        "job": "reper"
+    },
+]
+
+
+export default users;
--- L4-express.js/index.js
+++ L4-express.js/index.js
@@ -1,18 +1,44 @@
 import express from "express";
 import { readFile } from "fs/promises";
+import users from "./data.js";
 
 const app = express()
 
-app.use(express.static("public"))
+app.use(express.json())
 
-app.get("/", async (req, res) => {
-    const content = await readFile("./pages/index.html")
-    res.end(content)
+app.get("/users", (req, res) => {
+    res.writeHead(201, "ok!", {
+        "Content-Type": "application/json"
+    })
+
+    let data = users.filter((user) => req.query.name ? user.name.toLowerCase().startsWith(req.query.name.toLowerCase()) : true)
+
+    return res.status(200).end(JSON.stringify(data))
 })
-app.get('/about', async (req, res) => {
-    const content = await readFile("./pages/about.html")
-    res.end(content)
+app.post("/users", (req, res) => {
+    users.push(req.body)
+    return res.status(201).end('User has been created successfully!')
 })
+app.put("/users/:id", (req, res) => {
+    const _id = req.params.id
+    const indexOfUser = users.findIndex(({ id }) => id === _id)
+    users.splice(indexOfUser, 1, req.body)
+    res.writeHead(201, "ok!", {
+        "Content-Type": "application/json"
+    })
+    return res.end("User edited successfully!")
+})
+
+app.delete("/users/:id", (req, res) => {
+    const _id = req.params.id
+    const indexOfUser = users.findIndex(({ id }) => id === _id)
+    users.splice(indexOfUser, 1)
+    res.writeHead(200, "ok!", {
+        "Content-Type": "application/json"
+    })
+    return res.end("User deleted successfully!")
+})
+
 app.get('*', async (req, res) => {
     const content = await readFile("./pages/notfound.html")
     res.end(content)
