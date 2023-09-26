6d35a31a8716f3c31a0865f18f5a01a826446f19
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-09-26 20:11:03 +0400

    add html-simple-pages

--- L4-express.js/index.js
+++ L4-express.js/index.js
@@ -1,12 +1,21 @@
 import express from "express";
+import { readFile } from "fs/promises";
 
 const app = express()
 
-app.get("/", (req, res) => {
-    res.end('<h1> heyyy men burdayam.... </h1>')
+app.use(express.static("public"))
+
+app.get("/", async (req, res) => {
+    const content = await readFile("./pages/index.html")
+    res.end(content)
 })
-app.get('/about', (req, res) => {
-    res.end('<h1> About page</h1>')
+app.get('/about', async (req, res) => {
+    const content = await readFile("./pages/about.html")
+    res.end(content)
+})
+app.get('*', async (req, res) => {
+    const content = await readFile("./pages/notfound.html")
+    res.end(content)
 })
 
 app.listen(3000, () => {
