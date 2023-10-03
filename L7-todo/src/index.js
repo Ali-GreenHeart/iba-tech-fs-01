9974b5cfd72a42eaedd568dd008873b674198b74
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-10-03 22:16:53 +0400

    fg

--- L7-todo/src/index.js
+++ L7-todo/src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import todoRouter from "./routes/todo.route.js";
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express();
 app.use(express.json());
 app.use('/todo', todoRouter)
 
 
-app.listen(4000, () => {
-  console.log("server is up....");
+app.listen(process.env.PORT, () => {
+  console.log(`${process.env.PORT}-de server is up....`);
 });
