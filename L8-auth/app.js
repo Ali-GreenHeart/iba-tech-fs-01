25cf171d802ae2b1613c930e82e5067f8a09a980
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-10-05 21:04:02 +0400

    add adminJWT

--- L8-auth/app.js
+++ L8-auth/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import authMiddleware from './authMiddleware.js'
+import authMiddleware, { adminMiddleware } from './authMiddleware.js'
 import { users } from "./db.js"
 import { v4 as genId } from 'uuid'
 import jwt from 'jsonwebtoken'
@@ -10,18 +10,27 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 app.get('/products', (req, res) => {
-    return res.json(`Here the products...`)
+    return res.json(users)
 })
 app.post('/login', (req, res) => {
-    const { username, password } = req.body
+    const { username, password, role = 'user' } = req.body
     const id = genId()
     users.push({ id, username, password })
-    const token = jwt.sign({ id }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' })
+    const token = jwt.sign({ role, id }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' })
     return res.status(201).json({ token, id, msg: 'new user has been created!', })
 })
+
 app.post('/order-product', authMiddleware, (req, res) => {
     console.log(req.userId)
     return res.end('burdadi')
+})
+app.post('/edit-profile', authMiddleware, (req, res) => {
+    console.log(req.userId)
+    return res.end('bura edit-profile-dir')
+})
+app.post('/admin/add-product', adminMiddleware, (req, res) => {
+    console.log(req.userId)
+    return res.end('bura add product-dir')
 })
 
 app.listen(process.env.PORT, () => {
--- L8-auth/authMiddleware.js
+++ L8-auth/authMiddleware.js
@@ -1,5 +1,4 @@
-import { users } from "./db.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export default function authMiddleware(req, res, next) {
     const userSentToken = req.headers.authorization?.split(' ')[1]
@@ -11,6 +10,28 @@ export default function authMiddleware(req, res, next) {
             req.userId = user.id
             next()
         }
+    })
+}
 
+
+export function adminMiddleware(req, res, next) {
+    const adminSentToken = req.headers.authorization?.split(' ')[1]
+    if (!adminSentToken) return res.status(400).json({ err: 'Token is missing!' })
+
+    jwt.verify(adminSentToken, process.env.JWT_SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.status(400).json({ err: "token is wrong!" })
+        }
+        const urlContainsAdmin = req.url.includes('/admin')
+        if (user.role !== 'admin') {
+            if (urlContainsAdmin) {
+                return res.status(403).json({ err: 'ay usaq, isinle mesgul ol, emilere ilisme!!' })
+            }
+            return res.status(403).json({ err: "You're not an admin!" })
+        }
+        if (urlContainsAdmin && user.role === 'admin') {
+            res.isAdmin = true
+            next()
+        }
     })
 }
