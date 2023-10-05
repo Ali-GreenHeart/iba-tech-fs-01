cccd689ce60eaf14dd4972c62c7218a4b200e9aa
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-10-05 19:58:21 +0400

    add initial jwt

--- L8-auth/app.js
+++ L8-auth/app.js
@@ -1,16 +1,26 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authMiddleware from './authMiddleware.js'
+import { users } from "./db.js"
+import { v4 as genId } from 'uuid'
+import jwt from 'jsonwebtoken'
+
 dotenv.config()
 
 const app = express()
-
-
+app.use(express.json())
 app.get('/products', (req, res) => {
-    return res.json(`Hwere the products...`)
+    return res.json(`Here the products...`)
 })
-// middleware!
+app.post('/login', (req, res) => {
+    const { username, password } = req.body
+    const id = genId()
+    users.push({ id, username, password })
+    const token = jwt.sign({ id }, process.env.JWT_SECRET_KEY)
+    return res.status(201).json({ token, id, msg: 'new user has been created!', })
+})
 app.post('/order-product', authMiddleware, (req, res) => {
+    console.log(req.userId)
     return res.end('burdadi')
 })
 
--- L8-auth/authMiddleware.js
+++ L8-auth/authMiddleware.js
@@ -1,9 +1,16 @@
+import { users } from "./db.js";
+import jwt from "jsonwebtoken"
+
 export default function authMiddleware(req, res, next) {
-    const userSentToken = req.headers?.authorization?.split(' ')[1]
-    let token = "ali"; // hard-code
-    if (token === userSentToken) {
-        next()
-    } else {
-        return res.status(401).json({ err: 'Wrong credentials!' })
-    }
+    const userSentToken = req.headers.authorization?.split(' ')[1]
+    if (!userSentToken) return res.status(400).json({ err: 'Token is missing!' })
+    jwt.verify(userSentToken, process.env.JWT_SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.status(401).json({ err: 'Token is wrong!' })
+        } else {
+            req.userId = user.id
+            next()
+        }
+
+    })
 }
