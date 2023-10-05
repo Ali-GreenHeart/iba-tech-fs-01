295e2f3115afbfae7827ce0a34155d65df447726
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-10-05 21:17:17 +0400

    add bcrypt

--- L8-auth/app.js
+++ L8-auth/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import bcrypt from 'bcrypt'
 import authMiddleware, { adminMiddleware } from './authMiddleware.js'
 import { users } from "./db.js"
 import { v4 as genId } from 'uuid'
@@ -12,12 +13,24 @@ app.use(express.json())
 app.get('/products', (req, res) => {
     return res.json(users)
 })
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const { username, password, role = 'user' } = req.body
+    const generatedSalt = await bcrypt.genSalt(+process.env.BCRYPT_SALT_ROUNDS)
+    const hashPswd = await bcrypt.hash(password, generatedSalt)
     const id = genId()
-    users.push({ id, username, password })
+    users.push({ id, username, password: hashPswd })
     const token = jwt.sign({ role, id }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' })
     return res.status(201).json({ token, id, msg: 'new user has been created!', })
+})
+app.post('/edit-password', authMiddleware, async (req, res) => {
+    const id = req.userId
+    const { password } = req.body
+    const encrypted = users.find((user) => user.id === id).password
+    const pswdSame = await bcrypt.compare(password, encrypted)
+    if (pswdSame) {
+        return res.json("you're the same person!")
+    }
+    return res.status(401).json("Aldatma bizi...")
 })
 
 app.post('/order-product', authMiddleware, (req, res) => {
