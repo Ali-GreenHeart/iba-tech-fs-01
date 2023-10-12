54345cb46c8669e2e621f9398beeec6943b714ae
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-10-12 21:19:50 +0400

    initial version of messaging with ali

--- L11-socket/app.js
+++ L11-socket/app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
+        io.emit('chat message', msg);
     });
     socket.on('disconnect', () => {
         console.log('a friend left us!')
