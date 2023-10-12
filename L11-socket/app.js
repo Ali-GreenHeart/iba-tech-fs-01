8115c24c9bbb75e5453ba37b01833d5bb465d1be
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-10-12 21:49:21 +0400

    add nick functionality

--- L11-socket/app.js
+++ L11-socket/app.js
@@ -5,16 +5,18 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    io.emit('someone connected', 'hugs🤗! new friend is here!')
+    socket.on('chat message', ({ nick, msg }) => {
+        io.emit('chat message', `${nick}: ${msg}`);
     });
     socket.on('disconnect', () => {
-        io.emit('someone disconnected', 'a friend left us!')
+        io.emit('someone disconnected', `a friend left us!`)
     })
 });
 server.listen(3000, () => {
